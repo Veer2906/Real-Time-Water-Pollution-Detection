d572962dcfb83a36f4c677ba9867897411d5c906
Veer Mehta <72297798+Veer2906@users.noreply.github.com>
2023-10-14 19:44:43 +0530

    Model Files

--- mlp.py
+++ mlp.py
@@ -5,39 +5,39 @@ import xml.etree.ElementTree as ET
 import tensorflow as tf 
 from tensorflow import keras 
 from tensorflow.keras import layers
+from PIL import Image
 
-num_bounding_box_to_predict = 2
+num_bounding_box_to_predict = 4
 #image (720, 1280, 3)
+desired_shape = (720, 1280)
 
 
 def create_mlp_model(image_height, image_width, num_channels):
     model = keras.Sequential([
         layers.Flatten(input_shape=(image_height, image_width, num_channels)),
-        layers.Dense(512, activation='relu'),
-        layers.Dense(256, activation='relu'),
+        layers.Dense(64, activation='relu'),
+        layers.Dense(32, activation='relu'),
         layers.Dense(num_bounding_box_to_predict*4, activation='linear')
     ])
 
     return model
 
-def load_data(folder_path):
-        
-    image_folder_path = folder_path+"images"
-    annotation_folder_path = folder_path+"annotations"
+def load_data(folder_path, num_images_to_load, num_bounding_box_to_predict, desired_shape):
+    image_folder_path = os.path.join(folder_path, "images")
+    annotation_folder_path = os.path.join(folder_path, "annotations")
     image_files = os.listdir(image_folder_path)
-    num_images = min(len(image_files), 30)
+    num_images = min(len(image_files), num_images_to_load)
+
     images = []
-
-    for i in range(num_images):
-        image_path = os.path.join((image_folder_path), image_files[i])
-        img = cv2.imread(image_path)
-        images.append(img)
-        
-    xml_files = [f for f in os.listdir(annotation_folder_path) if f.endswith('.xml')]
-
     bounding_boxes = []
 
-    for xml_file in xml_files[0:30]:
+    for i in range(num_images):
+        image_path = os.path.join(image_folder_path, image_files[i])
+        img = cv2.imread(image_path)
+        img = cv2.resize(img, desired_shape, interpolation=cv2.INTER_AREA)
+        images.append(img)
+
+        xml_file = image_files[i].replace(".jpg", ".xml")
         xml_path = os.path.join(annotation_folder_path, xml_file)
         tree = ET.parse(xml_path)
         root = tree.getroot()
@@ -48,25 +48,44 @@ def load_data(folder_path):
             ymin = int(obj.find("bndbox/ymin").text)
             xmax = int(obj.find("bndbox/xmax").text)
             ymax = int(obj.find("bndbox/ymax").text)
-            image_bounding_box = image_bounding_box + [xmin, ymin, xmax, ymax]
+            image_bounding_box.extend([xmin, ymin, xmax, ymax])
 
-            if len(image_bounding_box) >= (num_bounding_box_to_predict*4):
+            if len(image_bounding_box) >= (num_bounding_box_to_predict * 4):
                 break
 
-        if len(image_bounding_box) < (num_bounding_box_to_predict*4):
-            image_bounding_box = image_bounding_box + [0, 0, 0, 0] * (num_bounding_box_to_predict - len(image_bounding_box)//4)
+        if len(image_bounding_box) < (num_bounding_box_to_predict * 4):
+            image_bounding_box.extend([0, 0, 0, 0] * (num_bounding_box_to_predict - len(image_bounding_box) // 4))
 
-        
         bounding_boxes.append(image_bounding_box)
-        return images, bounding_boxes
 
+    return np.array(images), np.array(bounding_boxes)
             
 if __name__ == "__main__":
     training_folder_path = "C:\\Users\\veera\\Desktop\\Veer\\Coding\\Python\\Veritas\\FloW_IMG\\FloW_IMG\\training\\"
-    training_images, training_bounding_boxes = load_data(training_folder_path)
-    print(training_images)
+    training_images, training_bounding_boxes = load_data(training_folder_path, 100, num_bounding_box_to_predict, desired_shape)
+
+    training_images = np.array(training_images)
+    training_bounding_boxes = np.array(training_bounding_boxes)
+    training_images = np.reshape(training_images, (-1, 720, 1280, 3))
+
+
+
+    model = create_mlp_model(720, 1280, 3)
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    model.fit(training_images, training_bounding_boxes, epochs=10, verbose=1)
+
+    del training_images
+    del training_bounding_boxes
 
     test_folder_path = "C:\\Users\\veera\\Desktop\\Veer\\Coding\\Python\\Veritas\\FloW_IMG\\FloW_IMG\\test\\"
-    test_images, test_bounding_boxes = load_data(test_folder_path)
-    print(test_images)
+    test_images, test_bounding_boxes = load_data(test_folder_path, 100, num_bounding_box_to_predict, desired_shape)
+    test_images = np.array(test_images)
+
+    training_bounding_boxes = np.array(training_bounding_boxes)
+    test_images = np.reshape(test_images, (-1, 720, 1280, 3))
+
+    evaluation = model.evaluate(test_images, test_bounding_boxes)
+    print(evaluation)
+
+    #def load_data(folder_path, num_images_to_load, num_bounding_box_to_predict, desired_shape):
 
